b07048594e6947eaa826544bd8c9b33b6a7ebe81
matheop <50113657+matheop@users.noreply.github.com>
2021-04-29 01:52:46 +0200

    [ch37] feat: buttons & links added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,16 @@
 	<section>In main</section>
 </main>
 
-<section>Out of main</section>
+<section>
+	<button class="fill-blue">Primary Fill</button>
+	<button class="outline-blue">Primary Outline</button>
+	<button class="fill-yellow">Secondary Fill</button>
+	<button class="outline-yellow">Secondary Outline</button>
+	<button class="link">Link Button</button>
+	<a href="#">default</a>
+	<a href="#" class="yellow-link">yell</a>
+	<a href="#" class="header-link">Header Link</a>
+</section>
 
 <style lang="scss">
 	main {
