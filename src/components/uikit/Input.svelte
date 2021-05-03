5533b79883f62ffbb1e5b6ae5af99ce97dab9cb8
matheop <matheo.pierini.pro@gmail.com>
2021-05-03 11:11:24 +0200

    [ch37] feat: inputs + default textarea added && new typos created

---
+++ src/components/uikit/Input.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	/* Svelte */
+	import { slide } from "svelte/transition";
+
+	/* Helpers */
+	import type { ValidationProperty } from "$helpers/interfaces/index";
+	import ErrorMessage from "$uikit/helpers/ErrorMessage.svelte";
+
+	export let value: string = "";
+	export let type: string = "text";
+	export let placeholder: string = "";
+	export let autocomplete: string = "";
+
+	//validation
+	let isInputClicked: boolean = false;
+	// export let validation: ValidationProperty;
+	export let errors: string[] = [];
+	export let isFormChecked: boolean = false;
+	export let isRequired: boolean = false;
+
+	$: displayError = isInputClicked || isFormChecked;
+</script>
+
+{#if type === "text"}
+	<input
+		type="text"
+		{placeholder}
+		bind:value
+		{autocomplete}
+		class:error={errors.length && displayError}
+		class:valid={!errors.length && value}
+		required={isRequired}
+		on:input={() => (isInputClicked = true)} />
+	{#if errors.length && displayError}
+		<ErrorMessage error={errors[0]} />
+	{/if}
+{:else if type === "email"}
+	<input
+		type="email"
+		{placeholder}
+		bind:value
+		autocomplete="email"
+		class:error={errors.length && displayError}
+		class:valid={!errors.length && value}
+		required={isRequired}
+		on:input={() => (isInputClicked = true)} />
+	{#if errors.length && displayError}
+		<ErrorMessage error={errors[0]} />
+	{/if}
+{:else if type === "tel"}
+	<input
+		type="text"
+		{placeholder}
+		bind:value
+		autocomplete="tel"
+		class:error={errors.length && displayError}
+		class:valid={!errors.length && value}
+		on:input={() => (isInputClicked = true)} />
+	{#if errors.length && displayError}
+		<ErrorMessage error={errors[0]} />
+	{/if}
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,7 @@
 <script>
+	import Input from "$uikit/Input.svelte";
+	import TextArea from "$uikit/TextArea.svelte";
+
 	const name: string = "WORLD";
 </script>
 
@@ -8,22 +11,37 @@
 	<section>In main</section>
 </main>
 
-<section>
-	<button class="fill-blue">Primary Fill</button>
-	<button class="outline-blue">Primary Outline</button>
-	<button class="fill-yellow">Secondary Fill</button>
-	<button class="outline-yellow">Secondary Outline</button>
-	<button class="link">Link Button</button>
-	<a href="#">default</a>
-	<a href="#" class="yellow-link">yell</a>
-	<a href="#" class="header-link">Header Link</a>
+<section class="global-container">
+	<div>
+		<h2>BUTTONS</h2>
+		<button class="fill-blue">Primary Fill</button>
+		<button class="outline-blue">Primary Outline</button>
+		<button class="fill-yellow">Secondary Fill</button>
+		<button class="outline-yellow">Secondary Outline</button>
+		<button class="link">Link Button</button>
+	</div>
+	<div>
+		<h2>LINKS</h2>
+		<a href="#">default</a>
+		<a href="#" class="yellow-link">yell</a>
+		<a href="#" class="header-link">Header Link</a>
+	</div>
+
+	<div>
+		<h2>INPUTS</h2>
+		<Input
+			placeholder="placeholder"
+			type="email"
+			autocomplete="email" />
+		<hr />
+		<TextArea />
+	</div>
 </section>
 
 <style lang="scss">
 	main {
 		background-color: $prim-500;
 		section {
-			width: 100%;
 			background-color: $sec-500;
 		}
 		h1 {
@@ -33,6 +51,14 @@
 
 	section {
 		width: 100%;
-		background-color: $error;
+		background-color: $grey-300;
+
+		div {
+			margin: 2rem 0;
+
+			& > :global(*) {
+				margin: $sp-100;
+			}
+		}
 	}
 </style>
